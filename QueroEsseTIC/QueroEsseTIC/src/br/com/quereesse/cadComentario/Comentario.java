19ad160dcde46e18853e6a2757dcd824e8b74548
RamonLzSouza <ramonsouza0309@gmail.com>
2019-06-09 14:37:55 -0300

    Getters e Setters criados na classe comentario, insere e remove e atualiza na classe ComentarioDao

--- QueroEsseTIC/QueroEsseTIC/src/br/com/quereesse/cadComentario/Comentario.java
+++ QueroEsseTIC/QueroEsseTIC/src/br/com/quereesse/cadComentario/Comentario.java
@@ -10,5 +10,33 @@ package br.com.quereesse.cadComentario;
  * @author vsued
  */
 public class Comentario {
+    private int idComentario;
+    private String comentario;
+    
+    
+    public Comentario(){        
+    }
+
+    public Comentario(int idComentario, String comentario) {
+        this.idComentario = idComentario;
+        this.comentario = comentario;
+    }
+
+    public int getIdComentario() {
+        return idComentario;
+    }
+
+    public void setIdComentario(int idComentario) {
+        this.idComentario = idComentario;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    
     
 }
