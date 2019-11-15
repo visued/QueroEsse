e14b30cc4d6842f3576ff452413a9795aa189b1b
Mateus Borges <mateusborges090@gmail.com>
2019-11-15 17:38:32 -0300

    Autocomplete

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import Main from '../template/Main'
 import Logo from '../../assets/imgs/logo-principal.png'
+import './Busca.css';
+import JSONP from 'jsonp';
 import { AutoComplete } from 'material-ui';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import JSONP from 'jsonp';
-import SearchBar from 'material-ui-search-bar';
-import './Busca.css';
+
+const getTheme = () => {
+  let overwrites = {
+        "palette": {
+            "primary1Color": "#c6c6c6",
+            "primary3Color": "##c6c6c6",
+            "accent3Color": "#c6c6c6",
+            "borderColor": "#c6c6c6"
+        }, 
+    };
+  return getMuiTheme(baseTheme, overwrites);
+}
 
 const googleAutoSuggestURL = `//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=`;
 
 class Busca extends React.Component {
+    
     constructor(props) {
         super(props);
         this.onUpdateInput = this.onUpdateInput.bind(this);
@@ -53,35 +66,34 @@ class Busca extends React.Component {
     }
 
     render() {
+        
         return (
             <div >
                 <Main>
                     <img src={Logo} alt="logo" />
                 </Main>
-                <MuiThemeProvider muiTheme={getMuiTheme()}>
-                    
-
-                    <SearchBar
-                        onChange={() => console.log('onChange')}
-                        onRequestSearch={() => console.log('onRequestSearch')}
-                        hintText= 'Encontre o seu produto...'
+                <MuiThemeProvider muiTheme={getTheme()}>
+                    <span  class="fa fa-search "></span>
+                    <AutoComplete 
+                        underlineStyle={{display: 'none'}}
+                        dataSource    = {this.state.dataSource}
+                        onUpdateInput = {this.onUpdateInput}
+                        fullWidth={true}
+                        placeholder = "Encontre o seu produto..."
                         style={{
-                            margin: '0 250px',
-                            maxWidth: 600
-                        }}
+                            margin: '0 255px',
+                            maxWidth: 560,
+                            border:' 1px solid #c6c6c6',
+                            height:'45px',
+                            'box-shadow': '0 1px 1px rgba(0,0,0,0.1)',
+                            'border-radius': '8px',
+                            '-webkit-box-shadow': '1px 2px 5px 1px rgba(0,0,0,0.22)',
+                            '-moz-box-shadow': '1px 2px 5px 1px rgba(0,0,0,0.22)',
+                            'text-indent': '8px'
+                        }} 
                     />
                 </MuiThemeProvider>
-
-                {/* <div >
-                <input type="text" className="busca"  
-                    name="busca"
-                    placeholder="Encontre o seu produto..." />
-                <span  class="fa fa-search "></span>
-                </div> */}
             </div>
-
-
-
         )
     }
 }
